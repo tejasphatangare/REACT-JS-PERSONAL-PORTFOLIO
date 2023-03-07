f651b60e34b70222dd02fc741da85764f06e1f66
Tejas Navinkumar Phatangare <124050638+tejasphatangare@users.noreply.github.com>
2023-03-07 13:47:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import About from './Templates/about us/About';
+import Blogs from './Templates/blogs/Blogs';
+import Contact from './Templates/contact/Contact';
+import Navbar from './Templates/navbar/Navbar';
+import Services from './Templates/service/Services';
+
+function App() {
+  return (
+  <>
+
+ <Navbar/>
+ <About/>
+ <Blogs/>
+ <Services/>
+ <Contact/>
+
+  </>
+  );
+}
+
+export default App;
